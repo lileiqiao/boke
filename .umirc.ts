6f3573d9af46b61399a38f897b9f92f8222776a9
李泪桥 <1879197419@qq.com>
2024-04-28 19:29:52 +0800

    修改

--- .umirc.ts
+++ .umirc.ts
@@ -1,6 +1,11 @@
 import { defineConfig } from "umi";
 
 export default defineConfig({
+  history: {
+    type: 'hash'
+  },
+  publicPath: process.env.NODE_ENV === 'production' ? './' : '/',
+
   routes: [
     { path: "/", component: "index" },
     { path: "/docs", component: "docs" },
